4246b68a90659a09c614f6fe36eecb06f625ff62
Christian Hermann <c.hermann@bitbeans.de>
2014-12-14 09:57:22 +0100

    Identation

--- Entropy/Entropy.py
+++ Entropy/Entropy.py
@@ -4,7 +4,7 @@
   http://plugins.serverdensity.com/entropy/
   https://github.com/serverdensity/sd-agent-plugins/
   Autor: @bitbeans
-  Version: 1.0.1
+  Version: 1.0.0
 """
 import json
 import sys
@@ -15,37 +15,38 @@ import time
 from decimal import *
 
 class Entropy (object):
-  def __init__(self, agentConfig, checksLogger, rawConfig):
-    self.agentConfig = agentConfig
-    self.checksLogger = checksLogger
-    self.rawConfig = rawConfig
-    self.version = platform.python_version_tuple()
 
-  def run(self):
-    data = {}
-    if platform.system() == 'Linux':
-      e = subprocess.check_output("cat /proc/sys/kernel/random/entropy_avail", shell=True)
-      data = {'available': float(e)}
-    else:
-      self.checks_logger.error('Plugin currently only available on Linux.')
-    return data
+    def __init__(self, agentConfig, checksLogger, rawConfig):
+        self.agentConfig = agentConfig
+        self.checksLogger = checksLogger
+        self.rawConfig = rawConfig
+        self.version = platform.python_version_tuple()
+
+    def run(self):
+        data = {}
+        if platform.system() == 'Linux':
+            e = subprocess.check_output("cat /proc/sys/kernel/random/entropy_avail", shell=True)
+            data = {'available': float(e)}
+        else:
+            self.checks_logger.error('Plugin currently only available on Linux.')
+        return data
 
 if __name__ == '__main__':
-  """Standalone test"""
-  raw_agent_config = {
-      'Entropy': {
-      }
-  }
+    """Standalone test"""
+    raw_agent_config = {
+        'Entropy': {
+        }
+    }
 
-  main_checks_logger = logging.getLogger('Entropy')
-  main_checks_logger.setLevel(logging.DEBUG)
-  main_checks_logger.addHandler(logging.StreamHandler(sys.stdout))
-  entropy_check = Entropy({}, main_checks_logger, raw_agent_config)
+    main_checks_logger = logging.getLogger('Entropy')
+    main_checks_logger.setLevel(logging.DEBUG)
+    main_checks_logger.addHandler(logging.StreamHandler(sys.stdout))
+    entropy_check = Entropy({}, main_checks_logger, raw_agent_config)
 
-  while True:
-    try:
-      print json.dumps(entropy_check.run(), indent=4, sort_keys=True)
-    except:
-      main_checks_logger.exception("Unhandled exception")
-    finally:
-      time.sleep(60)
+    while True:
+        try:
+            print json.dumps(entropy_check.run(), indent=4, sort_keys=True)
+        except:
+            main_checks_logger.exception("Unhandled exception")
+        finally:
+            time.sleep(60)
