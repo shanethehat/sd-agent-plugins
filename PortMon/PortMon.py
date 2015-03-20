2b00d0df073db6404b4eaceb3bbb3446c7037bc1
Harry Perks <harry.perks@serverdensity.com>
2015-03-20 10:31:04 +0000

    more pep8 stuff

--- PortMon/PortMon.py
+++ PortMon/PortMon.py
@@ -8,25 +8,25 @@ import socket
 class PortMon(object):
     '''
     Config should be in /etc/sd-agent/config.cfg like:
-    
+
     portmon_timeout: 5
     portmon_targets: 127.0.0.1:22,192.168.3.4:5678
-    
+
     Multiple targets to be monitored should be separated with a ','.
     '''
-    
+
     def __init__(self, agentConfig, checksLogger, rawConfig):
         self.agentConfig = agentConfig
         self.checksLogger = checksLogger
         self.rawConfig = rawConfig
-        
+
         self.timeout = int(rawConfig['Main'].get('portmon_timeout', 10))
         targets_str = rawConfig['Main'].get('portmon_targets', '').strip()
         if len(targets_str):
             self.targets = targets_str.split(',')
         else:
             self.targets = []
-        
+
     def run(self):
         target_times = {}
         for t in self.targets:
@@ -34,7 +34,7 @@ class PortMon(object):
             key = t.replace('.', '_')
             target_times[key] = self.timeConnect(host, int(port))
         return target_times
-        
+
     def timeConnect(self, host, port):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(self.timeout)
@@ -48,9 +48,9 @@ class PortMon(object):
         except (socket.error, socket.timeout):
             return 'down'
         return ret
-        
+
 if __name__ == "__main__":
-    
+
     # Test ports: one that's not accessible and another that's open on my
     # MacBook.
     fake_rawConfig = {
@@ -59,4 +59,3 @@ if __name__ == "__main__":
     }
     pm = PortMon({}, {}, fake_rawConfig)
     print pm.run()
-    
