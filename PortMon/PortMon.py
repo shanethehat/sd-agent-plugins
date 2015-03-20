89aa9517de561927c088a8310bd29f3b04545ab1
Harry Perks <harry.perks@serverdensity.com>
2015-03-20 10:23:30 +0000

    pep8

--- PortMon/PortMon.py
+++ PortMon/PortMon.py
@@ -16,45 +16,43 @@ class PortMon(object):
     '''
     
     def __init__(self, agentConfig, checksLogger, rawConfig):
-        self.agentConfig    = agentConfig
-        self.checksLogger   = checksLogger
-        self.rawConfig      = rawConfig
+        self.agentConfig = agentConfig
+        self.checksLogger = checksLogger
+        self.rawConfig = rawConfig
         
-        self.timeout    = int( rawConfig['Main'].get('portmon_timeout', 10) )
-        targets_str     = rawConfig['Main'].get('portmon_targets', '').strip()
+        self.timeout = int(rawConfig['Main'].get('portmon_timeout', 10))
+        targets_str = rawConfig['Main'].get('portmon_targets', '').strip()
         if len(targets_str):
             self.targets = targets_str.split(',')
         else:
             self.targets = []
         
-    
     def run(self):
         target_times = {}
         for t in self.targets:
             host, port = t.split(':')
-            target_times[t.replace('.','_')] = self.timeConnect(host, int(port))
+            key = t.replace('.', '_')
+            target_times[key] = self.timeConnect(host, int(port))
         return target_times
         
-    
     def timeConnect(self, host, port):
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.settimeout(self.timeout)
         t_init = time.time()
-        #ret = None
+        # ret = None
         try:
             s.connect((host, port))
             ret = time.time() - t_init
-            #s.recv(0)
+            # s.recv(0)
             s.close()
         except (socket.error, socket.timeout):
             return 'down'
         return ret
         
-        
 if __name__ == "__main__":
     
     # Test ports: one that's not accessible and another that's open on my
-    # MacBook.
+    # MacBook.
     fake_rawConfig = {
         'portmon_targets':  "127.0.0.1:8542,192.168.6.6:1200",
         'portmon_timeout':  "10"
